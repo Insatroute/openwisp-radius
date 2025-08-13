8d3ece25765f98d9809c93a6e6c3e8d2b2f07cd2
sagar-nexapp <sagar.p@nexapp.co.in>
2025-08-13 15:20:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,12 +35,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "git+https://github.com/Insatroute/openwisp-users.git@main#egg=openwisp_users",
-        # (
-        #     "openwisp-utils[rest,celery]"
-        #     " @ https://github.com/openwisp/openwisp-utils/tarball/1.2"
-        # ),
-        "git+https://github.com/Insatroute/openwisp-utils.git@main#egg=openwisp_utils[rest,celery]"
         "passlib~=1.7.1",
         "djangorestframework-link-header-pagination~=0.1.1",
         "weasyprint>=65,<67",
